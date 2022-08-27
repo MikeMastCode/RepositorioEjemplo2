ae47f161db26015f5afe4c009ba2f0213898f797
Michael Reyes <maicol16042019@gmail.com>
2022-08-26 21:20:55 -0500

    Comit del 26  ya con Frontend #2

--- src/main/java/Controller/ControllerFull.java
+++ src/main/java/Controller/ControllerFull.java
@@ -4,7 +4,7 @@ import Entidades.Ejemplo2.Empresa;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import service.Empresaservice;
 
@@ -15,10 +15,24 @@ import java.util.List;
 public class ControllerFull {
     @Autowired
     Empresaservice empresaService;
-    @GetMapping ({"/Empresas","/VerEmpresas"})
-    public String viewEmpresas(Model model){
-        List<Empresa> listaEmpresa=empresaService.getAllEmpresas();
-       model.addAttribute( attributeName: "emplist", listaEmpresas);
+    @GetMapping ({"/","/VerEmpresas"})
+    public String viewEmpresas(Model model, @ModelAttribute("mensaje")String mensaje){
+        List<Empresa> listaEmpresas=empresaService.getAllEmpresas();
+        model.addAttribute("emplist",listaEmpresas);
+        model.addAttribute("mensaje",mensaje);
        return "verEmpresas";//Llamamos al HTML
+    }
+    @GetMapping("/AgregarEmpresa")
+    public String nuevaEmpresa(Model model){
+        Empresa emp = new Empresa();
+        model.addAttribute("emp",emp);
+        return "agregarEmpresa";
+    }
+    @PostMapping("/GuardarEmpresa")
+    public String guardarEmpresa(Empresa emp, RedirectAttributes redirectAttributes){
+    if(empresaService.saveOrUpdateEmpresa(emp)==true){
+        return "redirect:/verEmpresa";
+    }
+    return "redirect:/agregarEmpresa";
     }
 }
